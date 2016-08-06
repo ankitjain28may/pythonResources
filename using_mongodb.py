750fc6098db507b2053cb8022fadadc8c5321351
ankitjain28may <ankitjain28may77@gmail.com>
2016-08-07 04:44:45 +0530

    pymongo in mongodb

--- using_mongodb.py
+++ using_mongodb.py
@@ -1,6 +1,19 @@
 from pymongo import MongoClient
-client = MongoClient()
-db=client.test
-col=db.store
-result=col.insert_one({"Ankit":"HelloWorld"});
-print(result.inserted_id)
+client = MongoClient()   
+db=client.test				# test is database name
+col=db.store				# store is the collection name in mongo
+
+#Insert Data in Mongo
+result=col.insert_one({"Ankit":"HelloWorld"});		# Insert json in mongo
+print(result.inserted_id)							# print the _id
+
+
+# Query data from Mongo
+result=col.find_one({"Ankit":"HelloWorld"});		# Query my data to find 
+print(result)
+
+
+# Query multiple data from Mongo
+result=col.find({"Ankit":"HelloWorld"});	 		# Return cursor		
+for re in result:
+	print(re)
