3768221f84ff371764f45f142c3f30f8883817fd
ankitjain28may <ankitjain28may77@gmail.com>
2016-11-20 03:39:16 +0530

    Delete extra file

--- Download from cmd using python/run.py.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-url = input("Enter the Download-Url\n")
-name = input("Enter the name of the File with the extension\n")
-try:
-    print("Downloading starts...\n")
-    urllib.request.urlretrieve(url, name)
-    print("Download completed..!!")
-except Exception as e:
-    print(e)
