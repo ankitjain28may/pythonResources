790d50c1f5dd8ad1283009f816dfea181ab75e51
ankitjain28may <ankitjain28may77@gmail.com>
2017-02-27 18:53:18 +0530

    Added face recognization

---
+++ opencv/faceRecognization/faceRecog.py
@@ -0,0 +1,37 @@
+import cv2
+import sys
+# from PIL import Image
+
+# Get user supplied values
+imagePath = sys.argv[1]
+# cascPath = sys.argv[2]
+
+
+cascadePath = "haarcascade_frontalface_default.xml"
+
+# Create the haar cascade
+faceCascade = cv2.CascadeClassifier(cascadePath)
+
+# Read the image
+image = cv2.imread(imagePath)
+image = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(10, 10),
+    maxSize=(100, 100)
+    # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+)
+
+print "Found {0} faces!".format(len(faces))
+
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+cv2.imshow("Faces found", image)
+cv2.waitKey(0)
